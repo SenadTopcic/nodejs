1367b02462d781faaedf101a21558c5964b9df93
Senad Topcic <senad.topcic@gmail.com>
2023-07-29 16:14:02 +0100

    1:35

--- src/app.js
+++ src/app.js
@@ -1,5 +1,69 @@
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const mongoose = require("mongoose");
 
-//import { v4 as uuidv4 } from "uuid";
+const Customer = require("./models/customers");
 
-console.log(uuidv4());
+const app = express();
+
+mongoose.set("strictQuery", false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+const customers = [
+  {
+    name: "Caleb",
+    industry: "music",
+  },
+  {
+    name: "John",
+    industry: "networking",
+  },
+  {
+    name: "Sal",
+    industry: "sports medicine",
+  },
+];
+
+const customer = new Customer({
+  name: "Senad",
+  industry: "IT"
+});
+ customer.save()
+app.get("/", (req, res) => {
+  res.send(customer);
+});
+
+app.get("/api/customers", (req, res) => {
+  console.log("get home paged");
+  res.send({ customers: customers });
+});
+
+app.post("/api/customers", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post("/", (req, res) => {
+  res.send("post request");
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(CONNECTION);
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
---
+++ src/models/customers.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+customerSchema = new mongoose.Schema({
+  name: String,
+  industry: String,
+});
+module.exports = mongoose.model("Customer", customerSchema);
